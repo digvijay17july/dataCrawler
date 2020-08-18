00f1841a97b608ebb465045cc2067d017177b7cf
unknown <digvijay17july@gmail.com>
2020-08-19 00:06:48 +0530

    migrate to react

---
+++ src/index.js
@@ -0,0 +1,50 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "assets/css/bootstrap.min.css";
+import "assets/scss/now-ui-kit.scss?v=1.4.0";
+import "assets/demo/demo.css?v=1.4.0";
+import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+// pages for this kit
+import Index from "views/Index.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LoginPage from "views/login/LoginPage.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import SignUpPage from "views/signUp/SignUpPage.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={(props) => <Index {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={(props) => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/aboutus"
+          render={(props) => <LandingPage {...props} />}
+        />
+        <Route
+          path="/profile-page"
+          render={(props) => <ProfilePage {...props} />}
+        />
+        <Route
+          path="/login"
+          render={(props) => <LoginPage {...props} />}
+        />
+        <Route
+          path="/signup"
+          render={(props) => <SignUpPage {...props} />}
+        />
+        <Redirect to="/login" />
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
